af08a9a30bd709704541732bb43936a4d3388d98
luisamorimqa <luis.amorim.qa@gmail.com>
2025-10-02 12:04:12 -0300

    create Specs file

---
+++ src/main/java/com/mantis/api/utils/Specs.java
@@ -0,0 +1,20 @@
+package com.mantis.api.utils;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class Specs {
+
+    private static final String BASE_URL = ConfigReader.getProperty("api.baseUrl");
+    private static final String TOKEN = ConfigReader.getProperty("api.token");
+
+    public static RequestSpecification authSpec() {
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .addHeader("Content-type", "application/json")
+                .addHeader("Authorization", TOKEN)
+                .addHeader("Accept", "*/*")
+                .build()
+                ;
+    }
+}
