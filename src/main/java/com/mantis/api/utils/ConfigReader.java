81023d6037f05f53ebf6a308d1ff3cea96cdf899
luisamorimqa <luis.amorim.qa@gmail.com>
2025-10-02 12:03:16 -0300

    create ConfigReader file

---
+++ src/main/java/com/mantis/api/utils/ConfigReader.java
@@ -0,0 +1,27 @@
+package com.mantis.api.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try(InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("application.properties")) {
+
+            if(input == null)
+                throw new RuntimeException("Não foi possível encontrar o arquivo application.properties. Verifique o nome especificado.");
+
+            properties.load(input);
+
+        } catch(IOException exception) {
+            throw new RuntimeException("Erro ao carregar arquivo application.properties", exception);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
