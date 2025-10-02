c109e62054686dd6ecd963d30f1afca24020256c
luisamorimqa <luis.amorim.qa@gmail.com>
2025-10-02 14:48:20 -0300

    using SharedData at ProjectDataTest.java

--- src/main/java/com/mantis/api/data/ProjectDataTest.java
+++ src/main/java/com/mantis/api/data/ProjectDataTest.java
@@ -2,6 +2,8 @@ package com.mantis.api.data;
 
 import com.mantis.api.dto.ProjectDTO;
 import com.mantis.api.dto.StatusDTO;
+import com.mantis.api.dto.ViewStateDTO;
+import com.mantis.api.utils.SharedData;
 
 import static com.mantis.api.utils.DataFaker.*;
 
@@ -10,12 +12,19 @@ public class ProjectDataTest {
     public static ProjectDTO setValue() {
         ProjectDTO projectDTO = new ProjectDTO();
         StatusDTO statusDTO = new StatusDTO();
+        ViewStateDTO viewStateDTO = new ViewStateDTO();
 
         statusDTO.setName(getProjectStatus());
+        viewStateDTO.setName(getProjectViewState());
 
         projectDTO.setName(getRandomString());
         projectDTO.setStatus(statusDTO);
+        projectDTO.setDescription(getRandomParagraph());
+        projectDTO.setFilePath("/" + getRandomString() + "/");
+        projectDTO.setViewState(viewStateDTO);
+        projectDTO.setEnabled(getProjectEnabled());
 
+        SharedData.put("projectDTO", projectDTO);
         return projectDTO;
     }
 }
