aac7f6d2cbe58ef598f65a4611ddc22474142158
luisamorimqa <luis.amorim.qa@gmail.com>
2025-10-02 14:15:25 -0300

    create StatusDTO.java

---
+++ src/main/java/com/mantis/api/dto/StatusDTO.java
@@ -0,0 +1,14 @@
+package com.mantis.api.dto;
+
+public class StatusDTO {
+
+    private String name;
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
