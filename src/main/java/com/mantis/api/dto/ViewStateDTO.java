6bf451590d4eb729f0dccae5b03150a67d86b9fc
luisamorimqa <luis.amorim.qa@gmail.com>
2025-10-02 14:15:59 -0300

    create ViewStateDTO.java

---
+++ src/main/java/com/mantis/api/dto/ViewStateDTO.java
@@ -0,0 +1,14 @@
+package com.mantis.api.dto;
+
+public class ViewStateDTO {
+
+    private String name;
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
