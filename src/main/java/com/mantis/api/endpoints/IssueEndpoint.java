8961a94f2f23a054ed282464e17111757a94b4ee
luisamorimqa <luis.amorim.qa@gmail.com>
2025-10-09 12:04:50 -0300

    add inline comments to improve code readability

--- src/main/java/com/mantis/api/endpoints/IssueEndpoint.java
+++ src/main/java/com/mantis/api/endpoints/IssueEndpoint.java
@@ -17,16 +17,20 @@ public class IssueEndpoint {
 
     public void postMinimalIssue() {
 
+        //Instância do endpoint
         ProjectEndpoint projectEndpoint = new ProjectEndpoint();
+        //Preenchimento do DTO que será usado para criação do projeto
         ProjectDTO projectDTOCriacao = ProjectDataTest.setValue();
-
+        //Criação do projeto
         projectEndpoint.backgroundPostProject(projectDTOCriacao);
 
+        //Busca de informações usadas para a criação do projeto
         ProjectDTO projectDTO = SharedData.get("projectDTO");
+        //Criação do DTO que será usado para a criação da issue, já com os dados do projeto criado
         MinimalIssueDTO minimalIssueDTO = IssueDataTest.setValueMinimalIssue(projectDTO);
 
+        //Execução de requisição de criação da Issue
         try {
-
             given()
                     .spec(Specs.authSpec())
                     .body(minimalIssueDTO)
@@ -37,14 +41,16 @@ public class IssueEndpoint {
                     .log().all()
                     .statusCode(HttpStatus.SC_CREATED)
             ;
-
         } catch (AssertionError error) {
+            //Em caso de qualquer exceção, a mensagem será exibida
             System.err.println("Falha no teste postMinimalIssue(): " + error.getMessage());
             throw error;
         }
     }
 
     public void getAllIssues() {
+
+        //Consulta de todas as Issues cadastradas
         try {
             given()
                     .spec(Specs.authSpec())
@@ -56,6 +62,7 @@ public class IssueEndpoint {
                     .body("issues", hasSize(greaterThan(0)))
             ;
         } catch (AssertionError error) {
+            //Em caso de qualquer exceção, a mensagem será exibida
             System.err.println("Falha no teste getAllIssues(): " + error.getMessage());
             throw error;
         }
@@ -73,22 +80,27 @@ public class IssueEndpoint {
         MinimalIssueDTO minimalIssueDTO = IssueDataTest.setValueMinimalIssue(projectDTO);
         backgroundPostIssue(minimalIssueDTO);
 
-        //Consulta da Issue cadastrada
-        given()
-                .spec(Specs.authSpec())
-                .when()
-                .get("/api/rest/issues/" + ISSUE_ID)
-                .then()
-                .log().all()
-                .statusCode(HttpStatus.SC_OK)
-                .body("issues[0].summary", equalTo(minimalIssueDTO.getSummary()))
-                .body("issues[0].description", equalTo(minimalIssueDTO.getDescription()))
-        ;
-
+        try {
+            //Consulta da Issue cadastrada
+            given()
+                    .spec(Specs.authSpec())
+                    .when()
+                    .get("/api/rest/issues/" + ISSUE_ID)
+                    .then()
+                    .log().all()
+                    .statusCode(HttpStatus.SC_OK)
+                    .body("issues[0].summary", equalTo(minimalIssueDTO.getSummary()))
+                    .body("issues[0].description", equalTo(minimalIssueDTO.getDescription()))
+            ;
+        } catch(AssertionError error) {
+            //Em caso de qualquer exceção, a mensagem será exibida
+            System.err.println("Falha no teste getAnIssue(): " + error.getMessage());
+            throw error;
+        }
     }
 
     public void backgroundPostIssue(MinimalIssueDTO minimalIssueDTO) {
-
+        //Criação de Issue e obtenção do ID para utilização em outros testes
         ISSUE_ID =
                 given()
                         .spec(Specs.authSpec())
